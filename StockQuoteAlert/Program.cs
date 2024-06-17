97f5ca3a9da3319481e16c6012eaff45ac78f09c
AlvaroJoseLopes <alvarojoselopes@hotmail.com>
2024-06-16 22:33:13 -0300

    feat: add main program that reads config and request Brapi

--- StockQuoteAlert/Program.cs
+++ StockQuoteAlert/Program.cs
@@ -1,2 +1,24 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿internal static class Program
+{
+    public static async Task Main(string[] args)
+    {
+        try
+        {
+            var cliArgs = CLI.Parser.Parse(args);
+            var settings = ProgramSettings.Parser.Parse("settings.json");
+            var client = new StockAPI.Client(settings!.Api!.Token);
+            while (true)
+            {
+                var price = await client.GetMarketPrice(cliArgs.targetStock);
+                Console.WriteLine(price);
+                Thread.Sleep(settings.Api.Delay);
+            }
+                
+        } 
+        catch (Exception e)
+        {
+            Console.WriteLine($"Exception: {e.Message}");
+            Environment.Exit(1);
+        }
+    }
+}
