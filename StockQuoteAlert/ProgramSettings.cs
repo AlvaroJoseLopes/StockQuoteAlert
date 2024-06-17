446fcc36c6ab580d03ee56c3b9ba42f5f06afba6
AlvaroJoseLopes <alvarojoselopes@hotmail.com>
2024-06-16 22:31:22 -0300

    feat: add API settings parser

---
+++ StockQuoteAlert/ProgramSettings.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Text.Json;
+namespace ProgramSettings;
+
+
+public class Settings
+{
+    public APISettings? Api { get; set; }
+    /*public SmtpSettings? Smtp { get; set; }*/
+
+    public class APISettings
+    {
+        public string Token { get; set; } = string.Empty;
+        public int Delay { get; set; } = 10; // in seconds
+    }
+/*
+    public class SmtpSettings
+    {
+
+    }
+*/
+
+}
+
+public class Parser
+{
+    public static Settings? Parse(string filePath)
+    {
+        string content = File.ReadAllText(filePath);
+        return JsonSerializer.Deserialize<Settings>(content);
+    }
+}
