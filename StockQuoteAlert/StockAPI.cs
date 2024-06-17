910216c52b9910aa5d202fd2eb20048735d56dac
AlvaroJoseLopes <alvarojoselopes@hotmail.com>
2024-06-16 22:32:08 -0300

    feat: add brapi client API

---
+++ StockQuoteAlert/StockAPI.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Text.Json.Nodes;
+namespace StockAPI;
+
+public class Client
+{
+    private string _token;
+    private HttpClient _client;
+
+    public Client(string token)
+    {
+        _token = token;
+        _client = new HttpClient();
+    }
+
+    public async Task<decimal> GetMarketPrice(string stock)
+    {
+        string content = String.Empty;
+        string url = Url(stock);
+
+        try
+        {
+            var response = await _client.GetAsync(url);
+            response.EnsureSuccessStatusCode();
+            content = await response.Content.ReadAsStringAsync();
+        }
+        catch (Exception e)
+        {
+            Console.WriteLine($"Failed to get {stock} market price");
+            Console.WriteLine($"Exception: {e.Message}");
+        }
+
+
+        return JsonNode.Parse(content)!["results"]![0]!["regularMarketPrice"]!.GetValue<decimal>();
+    }
+
+    private string Url(string stock)
+    {
+        return $"https://brapi.dev/api/quote/{stock}?token={_token}";
+    }
+}
+
+
