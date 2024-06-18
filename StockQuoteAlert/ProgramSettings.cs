d13d8c71ff506e5e65a7323de58080be783b62b2
AlvaroJoseLopes <alvarojoselopes@hotmail.com>
2024-06-17 21:52:13 -0300

    feat: add Smtp settings serializable object

--- StockQuoteAlert/ProgramSettings.cs
+++ StockQuoteAlert/ProgramSettings.cs
@@ -5,26 +5,37 @@ namespace ProgramSettings;
 
 public class Settings
 {
-    public APISettings? Api { get; set; }
-    /*public SmtpSettings? Smtp { get; set; }*/
+    public required APISettings Api { get; set; }
+    public required SmtpSettings Smtp { get; set; }
 
     public class APISettings
     {
-        public string Token { get; set; } = string.Empty;
+        public required string Token { get; set; }
         public int Delay { get; set; } = 10; // in seconds
     }
-/*
+
     public class SmtpSettings
     {
+        public required string Host { get; set; }
+        public int Port { get; set; } = 587; // Default for Mailtrap
+        public required string Username { get; set; }
+        public required string Password { get; set; }
+        public required string Sender { get; set; } = "noreply@stockalert.com";
 
+        public class TargetEmail
+        {
+            public required string Email { set; get; }
+            public required string Name { set; get; }
+        }
+        public required IList<TargetEmail> TargetEmails { get; set; }
     }
-*/
+
 
 }
 
 public class Parser
 {
-    public static Settings? Parse(string filePath)
+    public static Settings Parse(string filePath)
     {
         string content = File.ReadAllText(filePath);
         return JsonSerializer.Deserialize<Settings>(content);
