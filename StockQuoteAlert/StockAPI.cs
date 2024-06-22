e98f312df82ba421a8326a4e10d43b4204d3c532
AlvaroJoseLopes <alvarojoselopes@hotmail.com>
2024-06-22 19:38:39 -0300

    feat: async json parsing after fetching market price

--- StockQuoteAlert/StockAPI.cs
+++ StockQuoteAlert/StockAPI.cs
@@ -15,31 +15,26 @@ public class Client
 
     public async Task<decimal> GetMarketPrice(string stock)
     {
-        string content = String.Empty;
         string url = Url(stock);
-
-        try
-        {
-            var response = await _client.GetAsync(url);
-            response.EnsureSuccessStatusCode();
-            content = await response.Content.ReadAsStringAsync();
-        }
-        catch (Exception e)
-        {
-            Console.WriteLine($"Failed to get {stock} market price");
-            Console.WriteLine($"Exception: {e.Message}");
-        }
-
-        var parsedContent = JsonNode.Parse(content);
+        var response = await _client.GetAsync(url);
+        response.EnsureSuccessStatusCode();
+        
+        var responseStream = await response.Content.ReadAsStreamAsync();
+        ThrowExceptionIfNull(responseStream);
+        
+        var parsedContent = await JsonNode.ParseAsync(responseStream);
         ThrowExceptionIfNull(parsedContent);
+        
         var results = parsedContent!["results"];
         ThrowExceptionIfNull(results);
+        
         var priceField = results![0];
         ThrowExceptionIfNull(priceField);
+        
         var price = priceField!["regularMarketPrice"];
         ThrowExceptionIfNull(price);
+        
         var priceValue = price!.GetValue<decimal>();
-
         return priceValue;
     }
 
